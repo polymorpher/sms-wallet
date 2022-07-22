57d2a19d35974c14106a9d3aea1b9d699f4f5098
polymorpher <aaron@qokka.ai>
2022-07-22 02:26:17 -0700

    fix persistence

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Wallet from './pages/Wallet'
 import Recover from './pages/Recover'
 import paths from './pages/paths'
+import { persistStore } from 'redux-persist'
+import { useDispatch } from 'react-redux'
+import { FlexColumn, FlexRow } from './components/Layout'
 const Routes = () => {
+  const dispatch = useDispatch()
+  const [rehydrated, setRehydrated] = useState(false)
+  useEffect(() => {
+    const store = require('./state/store')
+    persistStore(store.default, null, () => {
+      setRehydrated(true)
+    })
+  }, [dispatch])
+  if (!rehydrated) {
+    return (
+      <FlexColumn style={{ width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+        <FlexRow style={{ width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+          Loading...
+        </FlexRow>
+      </FlexColumn>
+    )
+  }
+
   return (
     <BrowserRouter>
       <Switch>
