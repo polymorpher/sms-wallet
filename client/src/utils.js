0b45573a31df7ef63058888b68e8cb94abb70a93
polymorpher <aaron@qokka.ai>
2022-07-21 18:31:15 -0700

    add aes utils on client and window sizing util

---
+++ client/src/utils.js
@@ -0,0 +1,47 @@
+import AES from 'aes-js'
+import sharedUtils from '../../shared/utils'
+import { useState, useEffect } from 'react'
+
+export const utils = {
+  ...sharedUtils,
+  /**
+   *
+   * @param content - Uint8Array, multiple of 16 bytes
+   * @param encryptionKey - Uint8Array, 16 or 32 bytes
+   * @param iv - Uint8Array, 16 bytes
+   */
+  encrypt: (content, encryptionKey, iv) => {
+    // eslint-disable-next-line new-cap
+    const aes = new AES.ModeOfOperation.cbc(encryptionKey, iv)
+    return aes.encrypt(content)
+  },
+  decrypt: (encryptedContent, encryptionKey, iv) => {
+    // eslint-disable-next-line new-cap
+    const aes = new AES.ModeOfOperation.cbc(encryptionKey, iv)
+    return aes.decrypt(encryptedContent)
+  }
+}
+
+export function getWindowDimensions () {
+  const { innerWidth: width, innerHeight: height } = window
+  return {
+    width,
+    height
+  }
+}
+
+export function useWindowDimensions () {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+  const isMobile = !(windowDimensions.width >= 600)
+
+  useEffect(() => {
+    function handleResize () {
+      setWindowDimensions(getWindowDimensions())
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return { isMobile, ...windowDimensions }
+}
