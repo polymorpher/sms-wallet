f2278542a71572e32e65b4869570f7123c6e8e4b
polymorpher <aaron@qokka.ai>
2022-07-21 18:33:02 -0700

    routes and paths

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -3,16 +3,16 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Wallet from './pages/Wallet'
 import Recover from './pages/Recover'
-
+import paths from './pages/paths'
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         {/* <Route exact path='/' render={() => <Landing />} /> */}
-        <Route exact path='/' render={() => <Wallet />} />
-        <Route path='/signup' render={() => <Signup />} />
-        <Route path='/recover' render={() => <Recover />} />
-        <Redirect to='/' />
+        <Route exact path={paths.root} render={() => <Wallet />} />
+        <Route path={paths.signup} render={() => <Signup />} />
+        <Route path={paths.recover} render={() => <Recover />} />
+        <Redirect to={paths.root} />
       </Switch>
     </BrowserRouter>
   )
---
+++ client/src/pages/paths.js
@@ -0,0 +1,8 @@
+export const base = process.env.PUBLIC_URL || ''
+
+export default {
+  root: base + '/',
+  signup: base + '/signup',
+  wallet: base + '/wallet',
+  recover: base + '/recover',
+}
