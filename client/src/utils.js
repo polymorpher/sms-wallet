3bbfa040e11cef71ede2f1a9834762e4c021e197
polymorpher <aaron@qokka.ai>
2022-07-22 15:24:45 -0700

    toBalance, explorer

--- client/src/utils.js
+++ client/src/utils.js
@@ -22,7 +22,10 @@ export const utils = {
     return aes.decrypt(encryptedContent)
   },
   getRestoreUri: (p) => {
-    return `https://${config.rootUrl}${paths.recover}?p=${p}`
+    return `${config.rootUrl}${paths.recover}?p=${p}`
+  },
+  getExplorerUri: (txHash) => {
+    return config.explorer.replace('{{tx}}', txHash)
   },
   validBalance: (balance, allowFloat) => {
     if (typeof balance === 'number') { return true }
@@ -49,6 +52,20 @@ export const utils = {
     const formatted = sharedUtils.formatNumber(ones, maxPrecision)
     const fiat = (price || 0) * parseFloat(ones)
     const fiatFormatted = sharedUtils.formatNumber(fiat)
+    return { balance, formatted, fiat, fiatFormatted, valid: true }
+  },
+
+  toBalance: (formatted, price, decimals, maxPrecision) => {
+    if (!utils.validBalance(formatted, true)) {
+      return { balance: 0, formatted: '0', fiat: 0, fiatFormatted: '0', valid: false }
+    }
+    const balance = sharedUtils.toFraction(formatted, null, decimals)
+    let fiat, fiatFormatted
+    if (price !== undefined && price !== null) {
+      const f = parseFloat(formatted)
+      fiat = f * (price || 0)
+      fiatFormatted = utils.formatNumber(fiat, maxPrecision)
+    }
     return { balance, formatted, fiat, fiatFormatted, valid: true }
   },
 }
