eeca6e7923023bcd76a73fe4b6458fa7fee3b677
polymorpher <aaron@qokka.ai>
2022-07-23 20:30:51 -0700

    fix low port permission

--- server/deploy/enable.sh
+++ server/deploy/enable.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/node
 sudo cp sms-wallet-server.service /etc/systemd/system/sms-wallet-server.service
 sudo systemctl start sms-wallet-server
 sudo systemctl enable sms-wallet-server
