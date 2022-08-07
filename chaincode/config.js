c0fc2964caad7a8e2e595697a39a3e2e71ef52bf
polymorpher <aaron@qokka.ai>
2022-08-07 05:24:24 -0700

    fix empty env json parse exception

--- chaincode/config.js
+++ chaincode/config.js
@@ -23,5 +23,5 @@ module.exports = {
   reportGas: false,
   initialOperatorThreshold: process.env.INITIAL_OPERATOR_THRESHOLD,
   // use JSON.parse to parse the array of relayers
-  operators: JSON.parse(process.env.OPERATORS)
+  operators: JSON.parse(process.env.OPERATORS || '[]')
 }
