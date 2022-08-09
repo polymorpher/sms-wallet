52d109a1d6934bb29e276ccb1ee839f156f5d5e1
polymorpher <aaron@qokka.ai>
2022-08-08 18:43:53 -0700

    NFT apis

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,14 @@ import axios from 'axios'
 import Web3 from 'web3'
 import config from '../config'
 import BN from 'bn.js'
-
+import Constants from '../../../shared/constants'
+import Contract from 'web3-eth-contract'
+const IERC20 = require('../../abi/IERC20.json')
+const IERC20Metadata = require('../../abi/IERC20Metadata.json')
+const IERC721 = require('../../abi/IERC721.json')
+const IERC721Metadata = require('../../abi/IERC721Metadata.json')
+const IERC1155 = require('../../abi/IERC1155.json')
+const IERC1155MetadataURI = require('../../abi/IERC1155MetadataURI.json')
 const headers = ({ secret, network }) => ({
   'X-SECRET': secret,
   'X-NETWORK': network,
@@ -19,6 +26,19 @@ const apiBase = axios.create({
 })
 
 const web3 = new Web3(config.rpc)
+Contract.setProvider(web3.currentProvider)
+
+const getTokenContract = {
+  ERC20: (address) => new Contract(IERC20, address),
+  ERC721: (address) => new Contract(IERC721, address),
+  ERC1155: (address) => new Contract(IERC1155, address)
+}
+
+const getTokenMetadataContract = {
+  ERC20: (address) => new Contract(IERC20Metadata, address),
+  ERC721: (address) => new Contract(IERC721Metadata, address),
+  ERC1155: (address) => new Contract(IERC1155MetadataURI, address)
+}
 
 const apis = {
   web3: {
@@ -59,7 +79,43 @@ const apis = {
     getBalance: async ({ address }) => {
       const b = await web3.eth.getBalance(address)
       return new BN(b)
-    }
+    },
+
+    // returns Promise<BN>
+    getTokenBalance: async ({ address, contractAddress, tokenType = '', tokenId }) => {
+      if (!utils.isValidTokenType(tokenType)) {
+        throw new Error(`Unknown token type: ${tokenType}`)
+      }
+      const c = await getTokenContract[tokenType](contractAddress)
+      if (tokenType === 'ERC20') {
+        return c.methods.balanceOf(address).call()
+      } else if (tokenType === 'ERC721') {
+        const owner = await c.ownerOf(tokenId)
+        return owner === address ? new BN(1) : new BN(0)
+      } else if (tokenType === 'ERC1155') {
+        return c.methods.balanceOf(address, tokenId).call()
+      } else {
+        throw Error('unreachable')
+      }
+    },
+
+    getTokenMetadata: async ({ tokenType, contractAddress, tokenId }) => {
+      if (!utils.isValidTokenType(tokenType)) {
+        throw new Error(`Unknown token type: ${tokenType}`)
+      }
+      const c = await getTokenMetadataContract[tokenType](contractAddress)
+      let name, symbol, uri, decimals
+      if (tokenType === 'ERC20') {
+        [name, symbol, decimals] = await Promise.all([c.methods.name().call(), c.methods.symbol().call(), c.methods.decimals().call()])
+      } else if (tokenType === 'ERC721') {
+        [name, symbol, uri] = await Promise.all([c.methods.name().call(), c.methods.symbol().call(), c.methods.tokenURI(tokenId).call()])
+      } else if (tokenType === 'ERC1155') {
+        uri = await c.methods.uri(tokenId).call()
+      } else {
+        throw Error('unreachable')
+      }
+      return { name, symbol, uri, decimals: decimals && new BN(decimals).toNumber() }
+    },
   },
   server: {
     signup: async ({ phone, eseed, ekey, address }) => {
@@ -101,6 +157,11 @@ const apis = {
       const { data } = await apiBase.post('/request-complete', { address, signature, id, txHash })
       const { success } = data
       return success
+    }
+  },
+  nft: {
+    getCachedData: async (address, tokenType, contractAddress, tokenId) => {
+      return {}
     }
   }
 }
