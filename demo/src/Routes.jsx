cbfe79ee17800eaefbe53a0a9b87a1ea03125e77
polymorpher <aaron@qokka.ai>
2022-08-18 21:50:20 -0700

    proper demo routes

--- demo/src/Routes.jsx
+++ demo/src/Routes.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Landing from './pages/Landing'
 import CallDemo from './pages/CallDemo'
+import RequestDemo from './pages/RequestDemo'
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' render={() => <Landing />} />
-        <Route path='' render={() => <CallDemo />} />
+        <Route path='/call' render={() => <CallDemo />} />
+        <Route path='/request' render={() => <RequestDemo />} />
         <Redirect to='/' />
       </Switch>
     </BrowserRouter>
