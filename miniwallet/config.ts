83e51d10d0ef1bcd1a7968d1ceb2ff5a3abb58f7
John Whitton <john@johnwhitton.com>
2022-09-19 15:54:50 -0700

    Update config so that testing and deploy work on hardhat

--- miniwallet/config.ts
+++ miniwallet/config.ts
@@ -17,5 +17,13 @@ export default {
       initialUserLimit: ethers.utils.parseEther(process.env.ETH_LOCAL_MINIWALLET_INIITIAL_USER_LIMIT || '1000'),
       initialAuthLimit: ethers.utils.parseEther(process.env.ETH_LOCAL_MINIWALLET_INIITIAL_AUTH_LIMIT || '100')
     }
+  },
+  hardhat: {
+    miniWallet: {
+      initialOperatorThreshold: process.env.HARDHAT_MINIWALLET_INITIAL_OPERATOR_THRESHOLD || '10',
+      initialOperators: JSON.parse(process.env.HARDHAT_MINIWALLET_INITIAL_OPERATORS || '[]'),
+      initialUserLimit: ethers.utils.parseEther(process.env.HARDHAT_MINIWALLET_INIITIAL_USER_LIMIT || '1000'),
+      initialAuthLimit: ethers.utils.parseEther(process.env.HARDHAT_MINIWALLET_INIITIAL_AUTH_LIMIT || '100')
+    }
   }
 }
