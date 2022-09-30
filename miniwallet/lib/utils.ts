077f987f8d76bc866a21396c3066eaff1332b3e5
polymorpher <aaron@qokka.ai>
2022-09-29 18:21:16 -0700

    fix deployment utils

--- miniwallet/lib/utils.ts
+++ miniwallet/lib/utils.ts
@@ -1,30 +1,27 @@
 import config from '../config'
 import { ethers } from 'hardhat'
-import { promises as fs, constants as fsConstants, existsSync, mkdirSync } from 'fs'
+import { promises as fs, existsSync, mkdirSync } from 'fs'
 import path from 'path'
 
-export const BASE_TEN = 10
-
-export async function checkDeployed (hre, contract) {
-  // If we don't save deployments (e.g. for local networks) deployed = fals
-  if (!hre.network.saveDeployments) { return false }
-  const artifactsDirectory = config.artifactsDirectory + hre.network.name + '/'
-  const artifactName = 'MiniWallet'
-  const artifactFile = artifactsDirectory + artifactName + '.json'
+export async function isDeployed (hre, contractName) {
+  // If we don't save deployments (e.g. for local networks) deployed = false
+  if (!hre.network.saveDeployments) {
+    return false
+  }
   try {
-    if (!existsSync(artifactFile)) {
+    if (!existsSync(contractName)) {
       return false
     }
-    const contractArtifact = await hre.deployments.getExtendedArtifact(contract)
-    const currentDeployment = JSON.parse(await fs.readFile(artifactFile, { encoding: 'utf-8' }))
+    const contractArtifact = await hre.deployments.getExtendedArtifact(contractName)
+    const currentDeployment = JSON.parse(await fs.readFile(path.join(config.artifactsDirectory, hre.network.name, `${contractName}.json`), { encoding: 'utf-8' }))
     for (const implementation of currentDeployment.contract.implementations) {
       if (implementation.bytecodeHash === ethers.utils.id(contractArtifact.bytecode)) {
-        console.log(`Have already deployed this version of ${artifactName}`)
+        console.log(`Have already deployed this version of ${contractName}`)
         console.log(`Current Deployment: ${JSON.stringify(currentDeployment)}`)
         return true
       }
     }
-    console.log(`Have already deployed another version of ${artifactName}`)
+    console.log(`Have already deployed another version of ${contractName}`)
     console.log(`Current Deployment: ${JSON.stringify(currentDeployment)}`)
     return false
   } catch (ex) {
@@ -35,40 +32,32 @@ export async function checkDeployed (hre, contract) {
   }
 }
 
-export async function persistDeployment (hre, contract, contractAddress, proxy, proxyAddress) {
-  const saveArtifacts = hre.network.saveDeployments
-  const artifactsDirectory = config.artifactsDirectory + hre.network.name + '/'
-  const artifactName = 'MiniWallet'
-  const artifactFile = artifactsDirectory + artifactName + '.json'
-  // Persist Contract Information
-  const contractArtifact = await hre.deployments.getExtendedArtifact(contract)
-  //   console.log(`miniWalletImplementation: ${JSON.stringify(miniWalletImplementationArtifact)}`)
-  const proxyArtifact = await hre.deployments.getExtendedArtifact(proxy)
-  //   console.log(`miniWalletImplementation: ${JSON.stringify(miniProxyArtifact)}`)
+export async function persistDeployment (hre, contractName, contractAddress, proxyName, proxyAddress) {
+  const artifactsDirectory = path.join(config.artifactsDirectory, hre.network.name)
+  const contractArtifact = await hre.deployments.getExtendedArtifact(contractName)
+  const proxyArtifact = await hre.deployments.getExtendedArtifact(proxyName)
   const miniWalletArtifact = {
     contract: {
-      name: 'MiniWallet',
+      name: contractName,
       network: hre.network.name,
       implementations: [
         {
-          name: 'MiniWallet',
-          version: 1,
+          name: contractName,
+          version: 1, // TODO: auto-increment
           address: contractAddress,
           bytecodeHash: ethers.utils.id(contractArtifact.bytecode || '')
         }
       ],
       proxyContract: {
-        name: 'MiniProxy',
+        name: proxyName,
         address: proxyAddress,
         bytecodeHash: ethers.utils.id(proxyArtifact.bytecode || '')
       }
     }
   }
-  console.log(`MiniWalletArtifact: ${JSON.stringify(miniWalletArtifact)}`)
-  if (saveArtifacts) {
-    if (!existsSync(artifactsDirectory)) {
-      mkdirSync(artifactsDirectory, { recursive: true })
-    }
-    await fs.writeFile(artifactFile, JSON.stringify(miniWalletArtifact), { encoding: 'utf-8' })
+  console.log(`Saving miniWalletArtifact: ${JSON.stringify(miniWalletArtifact)}`)
+  if (hre.network.saveDeployments) {
+    mkdirSync(artifactsDirectory, { recursive: true })
+    await fs.writeFile(path.join(artifactsDirectory, `${contractName}.json`), JSON.stringify(miniWalletArtifact), { encoding: 'utf-8' })
   }
 }
