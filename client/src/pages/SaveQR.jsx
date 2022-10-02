44310483b8523253d33333d888edef605d05aadf
polymorpher <aaron@qokka.ai>
2022-10-01 21:30:59 -0700

    make SaveQR more a component, strip title

--- client/src/pages/SaveQR.jsx
+++ client/src/pages/SaveQR.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react'
 import { Address, BaseText, Desc, Title } from '../components/Text'
-import { FlexRow } from '../components/Layout'
+import { FlexColumn, FlexRow } from '../components/Layout'
 import { Button } from '../components/Controls'
 import html2canvas from 'html2canvas'
 import styled from 'styled-components'
@@ -8,13 +8,13 @@ import styled from 'styled-components'
 const QRImage = styled.img`
   border: 1px solid lightgrey;
   border-radius: 8px;
-  box-shadow: 0px 0px 10px lightgrey;
+  box-shadow: 0 0 8px lightgrey;
   width: 256px;
   height: 256px;
   object-fit: contain;
 `
 
-const SaveQR = ({ address, qrCodeData, onSaveQR, onDone }) => {
+const SaveQR = ({ phone, address, qrCodeData, onSaveQR, onDone }) => {
   const refQr = useRef()
   const [codeSaved, setCodeSaved] = useState(false)
 
@@ -33,7 +33,7 @@ const SaveQR = ({ address, qrCodeData, onSaveQR, onDone }) => {
     const blob = await capture()
     const element = document.createElement('a')
     element.href = URL.createObjectURL(blob)
-    element.download = `sms-wallet-${address}.png`
+    element.download = `sms-wallet-${phone.replace('+', '_')}-${address}.png`
     document.body.appendChild(element)
     element.click()
     URL.revokeObjectURL(element.href)
@@ -50,7 +50,10 @@ const SaveQR = ({ address, qrCodeData, onSaveQR, onDone }) => {
           src={qrCodeData}
           onClick={saveQR}
         />
-        <Address>{address}</Address>
+        <FlexColumn>
+          <Address style={{ fontSize: 11 }}>{address}</Address>
+          <Address>{phone}</Address>
+        </FlexColumn>
         <FlexRow style={{ justifyContent: codeSaved ? 'space-between' : 'center', width: '100%' }}>
           <Button onClick={saveQR}>Save Image</Button>
           {codeSaved && <Button onClick={onDone}>Done {' >>'}</Button>}
